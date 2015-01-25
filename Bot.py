02934dbbb66b7c4534993f5d3eca1f089c0719d9
Thomas Carroll <tcarroll@unclelee.ch>
2015-01-25 13:36:00 +0000

    Add countries to reponse
    also revert some changes cause by not using git

--- Bot.py
+++ Bot.py
@@ -3,12 +3,37 @@ import time
 import praw
 import sqlite3
 import re
+from random import randint
+
+username = "danbanbot"
+dontreply = ["Mattophobia"]
+password = ""
+useragent = "Responds to X banned from Y jokes in /r/nerdcubed. Based on an idea by /u/tokyorockz. Created by /u/alexratman by adapting /u/GoldenSights replybot code "
+redditsub = "nerdcubed"
+find_exp = r'(he|dan|nerd).{1,15}?(banned from)'
+bad_exp = r'many.{1,15}?places'
+subreddit_exp = r'th.{1,3}?sub.{0,8}?'
+maxposts = 100
+wait = 20
+reply = "He's banned from many places especially: "
+
+with open("r", "countries.csv") as countries:
+	list_of_countries = countries.readlines()
+
+sql = sqlite3.connect('sql.db')
+print('Loaded SQL Database')
+cur = sql.cursor()
+
+cur.execute('CREATE TABLE IF NOT EXISTS oldposts(ID TEXT)')
+print('Loaded old post table')
+
+sql.commit()
+
+print('Logging in to reddit...')
+r = praw.Reddit(useragent)
+r.login(username, password)
 
 def scanSub():
-	'''
-	Scans given subreddit
-	Posts if post does not exist in db
-	'''
     print('Scanning for jokes on /r/' + redditsub + '.')
     subreddit = r.get_subreddit(redditsub)
     posts = subreddit.get_comments(limit = maxposts)
@@ -22,9 +47,10 @@ def scanSub():
                 match = re.search(find_exp, pbody, re.IGNORECASE)
                 notmatch = re.search(bad_exp, pbody, re.IGNORECASE)
                 subredditbans = re.search(subreddit_exp, pbody, re.IGNORECASE)
-                if match and not notmatch and not subredditbans and postauthor.lower() != username.lower() and not postauthor.lower() in dontreplytome.lower:
+                if match and not notmatch and not subredditbans and postauthor.lower() != username.lower() and not postauthor.lower() in alternativebot.lower:
                     print('Found one! Replying to ' + pid + ' by ' + postauthor)
-                    post.reply(reply)
+                    random_no = randint(0, len(list_of_countries))
+                    post.reply(reply, list_of_countries[random_no])
                     print('Successfully commented')
                 else:
                     print('No posts or will not reply to self or replied to comment')
@@ -39,44 +65,6 @@ def scanSub():
             pass
     sql.commit()
 
-#USERNAME FOR BOT ACCOUNT HERE
-username = ""
-#INSERT USERNAMES FOR YOUR SOMEONE YOU DONT WANT TO REPLY TO HERE
-dontreplytome = ["Mattophobia"]
-#PASSWORD FOR BOT ACCOUNT HERE
-password = "" 
-#REQUIRED FOR REDDIT NOT TO BAN YOUR SODDING ACCOUNT.
-useragent = "!CHANGE THIS BEFORE RUNNING!"
-#THIS SUBREDDIT THAT THIS IS USED ON
-redditsub = "nerdcubed" 
-#REGEX TO FIND JOKES
-find_exp = r'(he|dan|nerd).{1,15}?(banned from)'
-#IF IT MATCHES THID DON'T REPLY
-bad_exp = r'many.{1,15}?places'
-subreddit_exp = r'th.{1,3}?sub.{0,8}?'
-#WHAT THE BOT REPLIES WITH
-reply = "He's banned from many places"
-#THE MAXIMUM AMOUNT OF POSTS FETCHED AT ONCE
-maxposts = 100
-
-
-#CREATE DB CONNECTION
-sql = sqlite3.connect('sql.db')
-print('Loaded SQL Database')
-cur = sql.cursor()
-
-#SET-UP TABLES
-cur.execute('CREATE TABLE IF NOT EXISTS oldposts(ID TEXT)')
-print('Loaded old post table')
-
-sql.commit()
-
-#LOGIN TO REDDIT
-print('Logging in to reddit...')
-r = praw.Reddit(useragent)
-r.login(username, password)
-
-
 while True:
     try:
         scanSub()
@@ -84,3 +72,4 @@ while True:
         traceback.print_exc()
     print('Running again in %d seconds \n' % wait)
     sql.commit()
+    time.sleep(wait)
